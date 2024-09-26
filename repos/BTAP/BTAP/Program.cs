81fd75424080a9581f1c365ee0380ead3b378c22
ASUKA_RISFIN <116791022+kieutanphuoc84tv@users.noreply.github.com>
2024-09-26 11:20:54 +0700

    Add files via upload

---
+++ repos/BTAP/BTAP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap 1 so nguyen duong n: ");
+        int n;
+
+    
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.Write("Vui long nhap 1 so nguyen duong: ");
+        }
+
+  
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine($"tong S = 1 + 2 + ... + {n} là: {sum}");
+    }
+}
---
+++ repos/chanvale/sochansole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace sochansole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Nhap vao mot so nguyen duong: ");
+            int soNguyen = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (soNguyen > 0)
+            {
+                if (soNguyen % 2 == 0)
+                {
+                    Console.WriteLine("So {0} la so chan.", soNguyen);
+                }
+                else
+                {
+                    Console.WriteLine("So {0} la so le.", soNguyen);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vui long nhap mot so nguyen duong.");
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
