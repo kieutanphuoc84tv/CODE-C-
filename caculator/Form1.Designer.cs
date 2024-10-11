8a7f07ad2e84995235b6f0ecca2bbb4349cd68ef
ASUKA_RISFIN <116791022+kieutanphuoc84tv@users.noreply.github.com>
2024-10-11 18:12:59 +0700

    Add files via upload

---
+++ caculator/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace caculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            btsqrt = new Button();
+            btMR = new Button();
+            btm = new Button();
+            btxoa = new Button();
+            btcong = new Button();
+            bt3 = new Button();
+            bt2 = new Button();
+            bt1 = new Button();
+            bttru = new Button();
+            bt6 = new Button();
+            bt5 = new Button();
+            bt4 = new Button();
+            btnhan = new Button();
+            bt9 = new Button();
+            bt8 = new Button();
+            bt7 = new Button();
+            btchia = new Button();
+            btconghoactru = new Button();
+            btbang = new Button();
+            bt0 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 52);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(411, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // btsqrt
+            // 
+            btsqrt.Location = new Point(12, 108);
+            btsqrt.Name = "btsqrt";
+            btsqrt.Size = new Size(76, 44);
+            btsqrt.TabIndex = 1;
+            btsqrt.Text = "sqrt";
+            btsqrt.UseVisualStyleBackColor = true;
+            btsqrt.Click += btsqrt_Click;
+            // 
+            // btMR
+            // 
+            btMR.Location = new Point(118, 108);
+            btMR.Name = "btMR";
+            btMR.Size = new Size(76, 44);
+            btMR.TabIndex = 2;
+            btMR.Text = "MR";
+            btMR.UseVisualStyleBackColor = true;
+            btMR.Click += btMR_Click;
+            // 
+            // btm
+            // 
+            btm.Location = new Point(231, 108);
+            btm.Name = "btm";
+            btm.Size = new Size(76, 44);
+            btm.TabIndex = 3;
+            btm.Text = "M+";
+            btm.UseVisualStyleBackColor = true;
+            btm.Click += btm_Click;
+            // 
+            // btxoa
+            // 
+            btxoa.Location = new Point(347, 108);
+            btxoa.Name = "btxoa";
+            btxoa.Size = new Size(76, 44);
+            btxoa.TabIndex = 4;
+            btxoa.Text = "C";
+            btxoa.UseVisualStyleBackColor = true;
+            btxoa.Click += btxoa_Click;
+            // 
+            // btcong
+            // 
+            btcong.Location = new Point(347, 203);
+            btcong.Name = "btcong";
+            btcong.Size = new Size(76, 44);
+            btcong.TabIndex = 8;
+            btcong.Text = "+";
+            btcong.UseVisualStyleBackColor = true;
+            btcong.Click += btcong_Click;
+            // 
+            // bt3
+            // 
+            bt3.Location = new Point(231, 203);
+            bt3.Name = "bt3";
+            bt3.Size = new Size(76, 44);
+            bt3.TabIndex = 7;
+            bt3.Text = "3";
+            bt3.UseVisualStyleBackColor = true;
+            bt3.Click += bt3_Click;
+            // 
+            // bt2
+            // 
+            bt2.Location = new Point(118, 203);
+            bt2.Name = "bt2";
+            bt2.Size = new Size(76, 44);
+            bt2.TabIndex = 6;
+            bt2.Text = "2";
+            bt2.UseVisualStyleBackColor = true;
+            bt2.Click += bt2_Click;
+            // 
+            // bt1
+            // 
+            bt1.Location = new Point(12, 203);
+            bt1.Name = "bt1";
+            bt1.Size = new Size(76, 44);
+            bt1.TabIndex = 5;
+            bt1.Text = "1";
+            bt1.UseVisualStyleBackColor = true;
+            bt1.Click += bt1_Click;
+            // 
+            // bttru
+            // 
+            bttru.Location = new Point(347, 276);
+            bttru.Name = "bttru";
+            bttru.Size = new Size(76, 44);
+            bttru.TabIndex = 12;
+            bttru.Text = "-";
+            bttru.UseVisualStyleBackColor = true;
+            bttru.Click += bttru_Click;
+            // 
+            // bt6
+            // 
+            bt6.Location = new Point(231, 276);
+            bt6.Name = "bt6";
+            bt6.Size = new Size(76, 44);
+            bt6.TabIndex = 11;
+            bt6.Text = "6";
+            bt6.UseVisualStyleBackColor = true;
+            bt6.Click += bt6_Click;
+            // 
+            // bt5
+            // 
+            bt5.Location = new Point(118, 276);
+            bt5.Name = "bt5";
+            bt5.Size = new Size(76, 44);
+            bt5.TabIndex = 10;
+            bt5.Text = "5";
+            bt5.UseVisualStyleBackColor = true;
+            bt5.Click += bt5_Click;
+            // 
+            // bt4
+            // 
+            bt4.Location = new Point(12, 276);
+            bt4.Name = "bt4";
+            bt4.Size = new Size(76, 44);
+            bt4.TabIndex = 9;
+            bt4.Text = "4";
+            bt4.UseVisualStyleBackColor = true;
+            bt4.Click += bt4_Click;
+            // 
+            // btnhan
+            // 
+            btnhan.Location = new Point(347, 352);
+            btnhan.Name = "btnhan";
+            btnhan.Size = new Size(76, 44);
+            btnhan.TabIndex = 16;
+            btnhan.Text = "*";
+            btnhan.UseVisualStyleBackColor = true;
+            btnhan.Click += btnhan_Click;
+            // 
+            // bt9
+            // 
+            bt9.Location = new Point(231, 352);
+            bt9.Name = "bt9";
+            bt9.Size = new Size(76, 44);
+            bt9.TabIndex = 15;
+            bt9.Text = "9";
+            bt9.UseVisualStyleBackColor = true;
+            bt9.Click += bt9_Click;
+            // 
+            // bt8
+            // 
+            bt8.Location = new Point(118, 352);
+            bt8.Name = "bt8";
+            bt8.Size = new Size(76, 44);
+            bt8.TabIndex = 14;
+            bt8.Text = "8";
+            bt8.UseVisualStyleBackColor = true;
+            bt8.Click += bt8_Click;
+            // 
+            // bt7
+            // 
+            bt7.Location = new Point(12, 352);
+            bt7.Name = "bt7";
+            bt7.Size = new Size(76, 44);
+            bt7.TabIndex = 13;
+            bt7.Text = "7";
+            bt7.UseVisualStyleBackColor = true;
+            bt7.Click += bt7_Click;
+            // 
+            // btchia
+            // 
+            btchia.Location = new Point(347, 420);
+            btchia.Name = "btchia";
+            btchia.Size = new Size(76, 44);
+            btchia.TabIndex = 20;
+            btchia.Text = "/";
+            btchia.UseVisualStyleBackColor = true;
+            btchia.Click += btchia_Click;
+            // 
+            // btconghoactru
+            // 
+            btconghoactru.Location = new Point(231, 420);
+            btconghoactru.Name = "btconghoactru";
+            btconghoactru.Size = new Size(76, 44);
+            btconghoactru.TabIndex = 19;
+            btconghoactru.Text = "+/-";
+            btconghoactru.UseVisualStyleBackColor = true;
+            btconghoactru.Click += btconghoactru_Click;
+            // 
+            // btbang
+            // 
+            btbang.Cursor = Cursors.SizeAll;
+            btbang.Location = new Point(118, 420);
+            btbang.Name = "btbang";
+            btbang.Size = new Size(76, 44);
+            btbang.TabIndex = 18;
+            btbang.Text = "=";
+            btbang.UseVisualStyleBackColor = true;
+            btbang.Click += btbang_Click;
+            // 
+            // bt0
+            // 
+            bt0.Location = new Point(12, 420);
+            bt0.Name = "bt0";
+            bt0.Size = new Size(76, 44);
+            bt0.TabIndex = 17;
+            bt0.Text = "0";
+            bt0.UseVisualStyleBackColor = true;
+            bt0.Click += bt0_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(435, 475);
+            Controls.Add(btchia);
+            Controls.Add(btconghoactru);
+            Controls.Add(btbang);
+            Controls.Add(bt0);
+            Controls.Add(btnhan);
+            Controls.Add(bt9);
+            Controls.Add(bt8);
+            Controls.Add(bt7);
+            Controls.Add(bttru);
+            Controls.Add(bt6);
+            Controls.Add(bt5);
+            Controls.Add(bt4);
+            Controls.Add(btcong);
+            Controls.Add(bt3);
+            Controls.Add(bt2);
+            Controls.Add(bt1);
+            Controls.Add(btxoa);
+            Controls.Add(btm);
+            Controls.Add(btMR);
+            Controls.Add(btsqrt);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button btsqrt;
+        private Button btMR;
+        private Button btm;
+        private Button btxoa;
+        private Button btcong;
+        private Button bt3;
+        private Button bt2;
+        private Button bt1;
+        private Button bttru;
+        private Button bt6;
+        private Button bt5;
+        private Button bt4;
+        private Button btnhan;
+        private Button bt9;
+        private Button bt8;
+        private Button bt7;
+        private Button btchia;
+        private Button btconghoactru;
+        private Button btbang;
+        private Button bt0;
+    }
+}
---
+++ caculator/Form1.cs
@@ -0,0 +1,188 @@
+﻿namespace caculator
+{
+    public partial class Form1 : Form
+    {
+        double result = 0;
+        string operation = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btsqrt_Click(object sender, EventArgs e)
+        {
+            result = Math.Sqrt(Double.Parse(textBox1.Text));
+            textBox1.Text = result.ToString();
+        }
+
+        private void btMR_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = result.ToString();
+        }
+
+        private void btm_Click(object sender, EventArgs e)
+        {
+            result = 0;
+            textBox1.Text = "0";
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            result = 0;
+            isOperationPerformed = false;
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "1";
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void btcong_Click(object sender, EventArgs e)
+        {
+            operation = "+";
+            result = Double.Parse(textBox1.Text);
+            isOperationPerformed = true;
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void bttru_Click(object sender, EventArgs e)
+        {
+            operation = "-";
+            result = Double.Parse(textBox1.Text);
+            isOperationPerformed = true;
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "7";
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void btnhan_Click(object sender, EventArgs e)
+        {
+            operation = "*";
+            result = Double.Parse(textBox1.Text);
+            isOperationPerformed = true;
+        }
+
+        private void bt0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "0")
+                textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void btbang_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    textBox1.Text = (result + Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                    textBox1.Text = (result - Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "*":
+                    textBox1.Text = (result * Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "/":
+                    if (textBox1.Text != "0")
+                        textBox1.Text = (result / Double.Parse(textBox1.Text)).ToString();
+                    else
+                        MessageBox.Show("Cannot divide by zero");
+                    break;
+            }
+            isOperationPerformed = false;
+        }
+
+        private void btconghoactru_Click(object sender, EventArgs e)
+        {
+            double temp = Double.Parse(textBox1.Text);
+            temp = -temp;
+            textBox1.Text = temp.ToString();
+        }
+
+        private void btchia_Click(object sender, EventArgs e)
+        {
+            operation = "/";
+            result = Double.Parse(textBox1.Text);
+            isOperationPerformed = true;
+        }
+    }
+}
